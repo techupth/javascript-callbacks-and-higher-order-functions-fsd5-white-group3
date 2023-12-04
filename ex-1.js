58f544ec789eacb1e9b10196a4af54794bf41680
Pacharatnkp <pachara.pplzz@gmail.com>
2023-12-04 12:14:49 +0700

    feat: submit exercise

--- ex-1.js
+++ ex-1.js
@@ -8,5 +8,8 @@ const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
-
+employeeSalaries.forEach(function (salary) {
+  const newSalary = salary + 5000;
+  newEmployeeSalaries.push(newSalary);
+});
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
